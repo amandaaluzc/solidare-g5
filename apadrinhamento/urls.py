853805a08613083cb5628a10f17d588f730059fd
eduaab <eduardoaa1901@gmail.com>
2025-04-18 20:54:56 -0300

    tela exibição e correções

--- apadrinhamento/urls.py
+++ apadrinhamento/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("criança/<int:crianca_id>/", views.detalhes_crianca, name="detalhes_crianca"),
     path("cadastro-padrinho/", views.registrar_padrinho, name="cadastro_padrinho"),
     path('' , views.homepage, name='home'),
-    path('lista-criancas/', views.lista_criancas, name='lista_criancas')
+    path('lista-criancas/', views.lista_criancas, name='lista_criancas'),
+    path('pagina-exibicao/', views.pagina_exibicao, name='pagina_exibicao'),
 ]
